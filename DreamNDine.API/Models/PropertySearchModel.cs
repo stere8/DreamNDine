e251f488ce74429afb8dcd46c06937410851c8fb
stere8 <twizoraph4247@gmail.com>
2024-03-22 20:17:12 +0100

    *Progress I hope

---
+++ DreamNDine.API/Models/PropertySearchModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DreamNDine.API.Models;
+
+public class PropertySearchModel
+{
+    public string location { get; set; }
+    public DateTime checkInDate { get; set; }
+    public DateTime checkOutDate { get; set; }
+}
--- DreamNDine.Client/Pages/Index.cshtml.cs
+++ DreamNDine.Client/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
+using System.Text;
+using System.Text.Json;
+using DreamNDine.API.Models;
 using DreamNDine.BLL.Services;
 using DreamNDine.BLL.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DreamNDine.Client.Pages
@@ -8,6 +12,7 @@ namespace DreamNDine.Client.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IHousingService _propertyService;
+        public PropertySearchModel SearchModel;
 
         public IndexModel(ILogger<IndexModel> logger,IHousingService housingService)
         {
@@ -17,9 +22,29 @@ namespace DreamNDine.Client.Pages
 
         public List<Property> Properties { get; set; }
 
-        public void OnGet()
+        public async Task<IActionResult> OnPost(PropertySearchModel searchData)
         {
-        }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(JsonSerializer.Serialize(searchData), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("https://127.0.0.1:1026/api/Property/search/", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var properties = JsonSerializer.Deserialize<List<Property>>(responseString); 
+
+                    TempData["searchResults"] = properties; // Assuming you have logic to read these in PropertyListView 
+                    return RedirectToPage("/PropertyListView"); 
+                }
+            }
+
+            return BadRequest(); // Or handle the error more descriptively
+        }
     }
 }
