cd7650158afb5679539d62edb7df6df86ce34398
stere8 <twizoraph4247@gmail.com>
2024-03-23 17:39:21 +0100

    *Clean up

--- DnDClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DnDClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
